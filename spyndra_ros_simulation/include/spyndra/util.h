6d4a4f440c98d1de8779d29d1d275dee5e8a315f
roboticistYan <yc3240@columbia.edu>
2017-09-02 00:05:50 -0400

    update mapping function between joint angle and pwm command
    Use figure 5 of spyndra paper as standard.

--- spyndra_ros_simulation/include/spyndra/util.h
+++ spyndra_ros_simulation/include/spyndra/util.h
@@ -7,15 +7,17 @@
 #define SPYNDRA_UTIL_H
 namespace spyndra
 {
+const double PI = 3.1415926;
 /* cmd_to_rad
  * Maps command of servo motors to joint angles in radians
  */
-double cmd_to_rad( int leg, double cmd )
+double cmd_to_rad( int leg, double pwm )
 {
   if (leg % 2 == 0) // Femur mapping function
-    return (cmd - 300.0) / 150.0 * M_PI /4.0 + 0.2;
+    return (pwm - 255.0) * PI / 450.0;
   else              // Tibia mapping function
-    return (350.0 - cmd ) / 150.0 * M_PI /4.0 + 0.2;
+    return (90.0 - 9.0/23.0 * ( pwm - 260.0 )) * PI / 180;
+
 }
 }
 
